8b6c6190d26461383afb6eac03e6249488556ca9
gerganasuv <gerganasuv@gmail.com>
2022-05-08 10:32:01 +0300

    Add project files.

---
+++ Summator.Tests/UnitTest1.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+
+namespace Summator.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test_Sum_TwoPossitiveNumbers() 
+        {
+            long result = Summator.Sum(new int[] { 5, 7 });
+            Assert.That(result == 12);
+        }
+
+        [Test]
+        public void Test_Sum_OnePossitiveNumbers()
+        {
+            long result = Summator.Sum(new int[] { 5 });
+            Assert.That(result == 5);
+        }
+
+        [Test]
+        public void Test_Sum_TwoNegativeNumbers()
+        {
+            long result = Summator.Sum(new int[] { -7,-9 });
+            Assert.That(result == -16);
+        }
+
+        [Test]
+        public void Test_Sum_EmptyArray()
+        {
+            long result = Summator.Sum(new int[] { });
+            Assert.That(result == 0);
+        }
+
+        [Test]
+        public void Test_Sum_BigValues()
+        {
+            long actual = Summator.Sum(new int[] { 2000000000, 2000000000 , 2000000000 });
+            Assert.AreEqual(6000000000, actual);
+        }
+
+        [Test]
+        public void Test_Average_TwoPossitiveNumbers()
+        {
+            double result = Summator.Average(new int[] { 5, 5 });
+            Assert.That(result == 5);
+        }
+
+        [Test]
+        public void Test_Average_TwoNumbersWithDecimal()
+        {
+            double result = Summator.Average(new int[] { 5, 6 });
+            Assert.AreEqual(5.5, result);
+        }
+    }
+}
---
+++ SummatorProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+            if (result == 22)
+                Console.WriteLine("TEST PASS");
+            else Console.WriteLine("TEST FAIL");     
+        }
+    }
+}
